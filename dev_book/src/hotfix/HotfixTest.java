ac6d842ec4e50ebb5d5d03712a639f09832fafe4
ahj5606 <ymtr149@gmail.com>
2020-03-23 15:44:25 +0900

    hotfix브랜치로 파일추가함. 3시43분

---
+++ dev_book/src/hotfix/HotfixTest.java
@@ -0,0 +1,8 @@
+package hotfix;
+
+public class HotfixTest {
+	public static void main(String[] args) {
+	//프로젝트명 -> 오른쪽 -> Remote -> switch to -> New branch -> hotfix 
+		System.out.println("hotfix 브랜치에서 추가한 소스임.");
+	}
+}
