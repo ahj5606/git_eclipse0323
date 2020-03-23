b9eae65945fec725eda8a8448bf4a46dcb69e4bb
ahj5606 <ymtr149@gmail.com>
2020-03-23 17:59:44 +0900

    tomato추가

--- dev_book/src/tomato/tomato.java
+++ dev_book/src/tomato/tomato.java
@@ -4,4 +4,5 @@ public class tomato {
 	public static void main(String[] args) {
 		System.out.println("tomato 추가");
 	}
-}
+}   
+    
--- dev_book/src/tomato/tomato2.java
+++ dev_book/src/tomato/tomato2.java
@@ -2,6 +2,6 @@ package tomato;
 
 public class tomato2 {
 	public static void main(String[] args) {
-		System.out.println("tomato2 추가");
+		System.out.println("tomato2 추가"); 
 	}
 }
