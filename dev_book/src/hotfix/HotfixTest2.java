704dd567578565328c4e41317cd351819600352f
ahj5606 <ymtr149@gmail.com>
2020-03-23 16:10:48 +0900

    HotfixTest2.java 수정했음

--- dev_book/src/hotfix/HotfixTest2.java
+++ dev_book/src/hotfix/HotfixTest2.java
@@ -3,6 +3,8 @@ package hotfix;
 public class HotfixTest2 {
 	public static void main(String[] args) {
 	//hotfix브랜치에서 저장점을 한개 더 가지기 위한 소스 추가.
+		
 		System.out.println("hotfix 브랜치에서 2번째로 추가한 소스임.");
+		System.out.println("hotfix 에서 수정했음.");
 	} 
 }
